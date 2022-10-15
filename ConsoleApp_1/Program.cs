f2e7699cbc72c50d309e492203560adc2e641e78
zirtuff <aleev.alexandr@gmail.com>
2022-10-15 20:20:57 +0600

    ConsoleApp_1

--- ConsoleApp_1/Program.cs
+++ ConsoleApp_1/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает число и выдает его квадрат (число умноженное само на себя).
+
+//Запрос числа от пользователя
+Console.Write("Введите чило:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+//Вычисление квадрата исходного числа
+int sqrNumber = number * number;
+
+//Вывод результата на экран
+Console.WriteLine($"Квадрат вашего чила = {sqrNumber}");
+
