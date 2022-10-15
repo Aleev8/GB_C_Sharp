acea3196343416c7f2481cf4d86d0770530e48b4
zirtuff <aleev.alexandr@gmail.com>
2022-10-15 21:26:50 +0600

    Write ConsoleApp_4

---
+++ ConsoleApp_4/Program.cs
@@ -0,0 +1,11 @@
+﻿/* Напишите программу, которая на вход принимает одно число (N), а на
+выходе показывает все целые числа в промежутке от -N до N. */
+Console.Write("Введите число N:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int negativeN = n * -1;
+while(negativeN <= n)
+{
+    Console.WriteLine(negativeN);
+    negativeN++;
+}
