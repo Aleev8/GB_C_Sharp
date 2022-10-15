ad8b043cf81cc35ed8d555f23cbe00a3adc31627
zirtuff <aleev.alexandr@gmail.com>
2022-10-15 20:57:28 +0600

    Write ConsoleApp_2

--- ConsoleApp_2/Program.cs
+++ ConsoleApp_2/Program.cs
@@ -1,2 +1,14 @@
 ﻿// Напишите программу, которая принимает на фход два числа и вычисляет, не является ли одно крвадратом другого.
 
+Console.Write("Введите чило №1:");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите чило №2:");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberA == numberB * numberB)
+{
+    Console.WriteLine($"True: число А = {numberA} является квадратом Б = {numberB}");
+}
+else
+{
+    Console.WriteLine($"False: число А = {numberA} НЕ является квадратом Б = {numberB}");
+}
